ca4735a36eb8fff51831b382d4b6eee9bc393761
Cristiann2112 <cristianandresg2002@gmail.com>
2021-12-01 21:31:01 -0400

    Examen Poo

---
+++ Examen Poo.rb
@@ -0,0 +1,18 @@
+class Estudiante
+def __init__ (Nombre, Apellido,Cedula,Edad )
+def Inscribirmateria
+def Retirarmateria
+def Mostrarcursosinscritos    
+
+class Egresado(Estudiante)
+     #Atributos
+     def __init__ (Aniodeegreso)
+    
+class Retirado(Estudiante)
+    #Atributos
+    def __init__ (Fechaderetiro)
+ 
+ class Nograduado(Estudiante)
+      #Atributos
+      def __init__(Nograduado)
+      def __init__(Fechaderetiro)
