6f7106fd1f382b64273c48bb4087d37e61cc2262
Cristiann2112 <cristianandresg2002@gmail.com>
2021-12-01 22:36:32 -0400

    Update Examen Poo.py

--- Examen Poo.py
+++ Examen Poo.py
@@ -1,31 +1,51 @@
 class Estudiante
-def __init__ (Nombre, Apellido,Cedula,Edad )
-self.Nombre = Nombre 
-self.Apellido = Apellido
-self.Cedula = Cedula
-self.Edad = Edad 
+__materia = int 
 
-def Inscribirmateria (self)
-materia = input(Ingrese una Materia)
-def Retirarmateria (self)
-def Mostrarcursosinscritos (self)    
-print "Cursos Inscritos"
-print materia 
 
-class Egresado(Estudiante)
-     #Atributos
-     def __init__ (Aniodeegreso)
+ def __init__(self, cedulaDeIdentidad, Nombre, Apellido):  
+        self.cedulaDeIdentidad = cedulaDeIdentidad
+        self.nombre = Nombre
+        self.apellido = Apellido
+
+
+
+      def Inscribirmateria (self): 
+           vResult = self.__materia
+             print  "El  estudiante", self.nombre+self.apellido +
+              "Esta cursando la materia de:", self.__materia)
+        return vResult
+
+        def asignarlamateria(self, vMateria): 
+        if vMateria < 0:
+           
+        
+        else:
+            self.__materia = vMateria
     
-class Retirado(Estudiante)
-    #Atributos
-    def __init__ (Fechaderetiro)
- 
- class Nograduado(Estudiante)
+       def Retirarmateria (self)
+      def Mostrarcursosinscritos (self)    
+      print "Cursos Inscritos"
+     print materia 
+
+class Egresado(Estudiante):
+       #Atributos
+     def  Aniodeegreso (self):
+          pass
+    
+class Retirado(Estudiante):
       #Atributos
-      def __init__(Nograduado)
-      def __init__(Fechaderetiro)
+    def  Fechaderetiro (self):
+         pass
+ 
+class Nograduado(Estudiante):
+      #Atributos
+      def Nograduado (self):
+           pass
+      def Fechaderetiro (self):
+           pass
+    
 
 
-Nuevoestudiante.estudiante
-inscribirmateria.estudiante
-mostrarcursosinscritos.estudiante 
+CristianGonzalez = Nograduado("30123456", "Cristian", "Gonzalez")
+
+
