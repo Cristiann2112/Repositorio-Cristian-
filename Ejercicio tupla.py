acd3d414b989a674bdf7951f78a9be82217de0a2
Cristiann2112 <93207649+Cristiann2112@users.noreply.github.com>
2022-03-28 16:50:31 -0400

    Add files via upload

---
+++ Ejercicio tupla.py
@@ -0,0 +1,15 @@
+# 4.1 Implemente una tupla de un solo elemento. Tiene una particularidad, investigue. (2pts.)
+# Utilice mi cedula de identidad como tupla.
+tupla = (30075025)
+print (tupla)
+
+#4.2 Dada una lista de las edades de una población que ha ido a vacunarse,
+
+# Elimine de la lista, todas las ocurrencias del entero 10 (3pts)
+vEdades = ["2", "7", "58", "7", "45", "26", "10", "8", "56", "57", "97", "19", "11", "53", "3", "99", "62", "78", "29", "9", "37", "42", "56", "86", "28", "86", "95", "26", "49", "67", "21", "815", "67", "10", "58", "512", "24", "92", "89", "67", "53", "10", "9", "83", "1", "44", "10", "77", "98", "73", "57"]
+
+del vEdades[6]
+del vEdades[32]
+del vEdades[39]
+del vEdades[43]
+print(vEdades)
