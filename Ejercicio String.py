43481eacb3442b4e94531588d2d39c76ae749740
Cristiann2112 <93207649+Cristiann2112@users.noreply.github.com>
2022-05-16 12:25:20 -0400

    Add files via upload

---
+++ Ejercicio String.py
@@ -0,0 +1,13 @@
+# Desarrolle una función en python que acepte una variable string como primer parámetro 
+# y la cantidad de caracteres de como segundo parámetro. 
+# La función debe retornar un nuevo string que consista en el string original 
+# y el número correcto de caracteres necesarios para que el string se salga centrado
+# No agregue caracteres al final del string.
+saludo = "Hola profesor" 
+espacio = ""
+nombre= " soy Cristian"
+print(saludo + espacio + nombre)
+
+print(len(nombre))
+print(nombre.center(100))
+
